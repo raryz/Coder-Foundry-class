16a504bfde5586f0b5f872c4539254ff338ef8b9
raryz <raryz21@gmail.com>
2015-05-28 23:38:18 -0400

    dashboard work

---
+++ BudgetToolRAR/BudgetToolRAR/Models/DashBrdViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetToolRAR.Models
+{
+    public class DashBrdViewModel
+    {
+        public DashBrdViewModel()
+        {
+            accountInfo = new List<AccountInfo>();
+            transactionInfo = new List<TransactionInfo>();
+        }
+
+        public List<AccountInfo> accountInfo { get; set; }
+        public List<TransactionInfo> transactionInfo { get; set; }
+    }
+
+    public class AccountInfo
+    {
+        public string AccountName { get; set; }
+        public decimal AccountBalance { get; set; }
+        public decimal ReconciledBalance { get; set; }
+    }
+
+    public class TransactionInfo
+    {
+        public DateTimeOffset Date { get; set; }
+        public string AcctName { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+    }
+
+}
